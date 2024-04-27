5f2fa6801c26672e37917511cba418fa298780ad
kampamba shula <kampambashula@gmail.com>
2024-04-27 15:14:16 +0200

    Bootstrap version

--- voting/urls.py
+++ voting/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # URL for the home page
-    path('register/', views.register, name='register'),  # URL for voter registration
-    path('vote/', views.vote, name='vote'),  # URL for casting a vote
-    path('thank-you/', views.thank_you, name='thank_you'),  # URL for thank you page
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register_voter'),
+    path('vote/', views.vote, name='cast_vote'), # URL for thank you page
 ]
--- voting_system/settings.py
+++ voting_system/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'voting/static'),
+]
+
